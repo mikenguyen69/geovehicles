47137a3ad3475ed4e62348cc7d32d74ad283516c
Mike Nguyen <nguyenduchung0609@gmail.com>
2020-01-28 14:19:01 +1300

    Some refactor and styling

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -1,36 +1,54 @@
-import React from "react";
+import React, {useContext} from "react";
 import {GoogleLogin} from 'react-google-login';
 import { withStyles } from "@material-ui/core/styles";
 import {GraphQLClient} from 'graphql-request';
-// import Typography from "@material-ui/core/Typography";
+import Typography from "@material-ui/core/Typography";
+import {ME_QUERY} from '../../graphql/queries';
 
-const ME_QUERY = `
-{
-  me {
-    _id
-    name
-    email
-    picture
-  }
-}
-`
+import Context from '../../context';
+
 
 const Login =  ({ classes }) => {
 
+  const {dispatch} = useContext(Context)
+
   const onSuccess = async googleUser => {
-    //console.log(googleUser);
-    const idToken = googleUser.getAuthResponse().id_token;
-    //console.log(idToken);
-    const client = new GraphQLClient('http://localhost:4000/graphql', {
-      headers: {authorization: idToken}
-    })
-    const data = await client.request(ME_QUERY);
-    console.log(data);
+   
+    try {
+      const idToken = googleUser.getAuthResponse().id_token;
+    
+      const client = new GraphQLClient('http://localhost:4000/graphql', {
+        headers: {authorization: idToken}
+      })
+      const {me} = await client.request(ME_QUERY);
+
+      dispatch({type: "LOGIN_USER", payload: me})
+    } catch(err) {
+      onFailure(err);
+    }
   }
 
-  return <GoogleLogin clientId="356546691800-bchs3s6p3ddhv62shjk665ncsnl02ntc.apps.googleusercontent.com" onSuccess={onSuccess} 
-  isSignedIn="true"
-  />;
+  const onFailure = err => {
+    console.error("Error logging in", err);
+  }
+
+  return (
+    <div className={classes.root}>
+      <Typography 
+        component="h1" 
+        variant="h3"
+        gutterBottom noWrap
+        style={{color: "rgb(66,133,244)"}}>
+        Welcome
+      </Typography>
+      <GoogleLogin clientId="356546691800-bchs3s6p3ddhv62shjk665ncsnl02ntc.apps.googleusercontent.com" 
+      onSuccess={onSuccess} 
+      isSignedIn="true"
+      onFailure={onFailure}
+      />
+    </div>
+    
+  );
 };
 
 const styles = {
