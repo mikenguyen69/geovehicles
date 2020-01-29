10f4d9b0420fe84fd8268ae4a4a77af3e132c716
Mike Nguyen <nguyenduchung0609@gmail.com>
2020-01-30 11:37:57 +1300

    Add pin in real time

--- client/src/reducer.js
+++ client/src/reducer.js
@@ -47,6 +47,15 @@ export default function reducer(state, {type, payload}) {
                 pins: payload
             }
          
+        case "CREATE_PIN": 
+            const newPin = payload
+            const prevPins = state.pins.filter(pin => pin._id !== newPin._id)
+
+            return {
+                ...state,
+                pins: [...prevPins, newPin]
+            }
+
         default: 
             return state;        
     }
